ecd779de9ae495796300e2711201906729f6b4a1
Mathias Reker <mathias@reker.dk>
2021-04-15 13:52:03 +0200

    wip student

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -1,7 +1,17 @@
 package com.app;
 
+import com.app.model.Examination;
+import com.app.model.Student;
+
 public class Main {
   public static void main(String[] args) {
-    new App().run();
+    //new App().run();
+    Examination examination = new Examination("Matematik");
+    Student student = new Student("Mathias");
+
+    student.addExams(examination);
+    student.save();
+
+
   }
 }
