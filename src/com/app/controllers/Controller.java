487882181fd20d43fdcbdfa34a41e16cb49989ae
Mathias Reker <mathias@reker.dk>
2021-04-18 12:05:37 +0200

    Add more examples

--- src/com/app/controllers/Controller.java
+++ src/com/app/controllers/Controller.java
@@ -12,7 +12,9 @@ public class Controller {
     student.addExam(SchoolSubject.MATH, new double[] {0.25, 0.75}, new int[] {7, 7});
     student.addExam(SchoolSubject.PHYSICS, new double[] {0.25, 0.75}, new int[] {10, 12});
     student.addExam(SchoolSubject.GEOGRAPHY, 7);
-    student.addExam(SchoolSubject.ART,0);
+    student.addExam(SchoolSubject.ART, 0);
+    student.addExam(SchoolSubject.RELIGION, 12);
+    student.addExam(SchoolSubject.MUSIC, -3);
 
     view.displayStudent(student.getName(), student.result());
     view.displayAverage(student.calculateAverage());
@@ -23,6 +25,8 @@ public class Controller {
     student2.addExam(SchoolSubject.PHYSICS, new double[] {0.25, 0.75}, new int[] {10, 12});
     student2.addExam(SchoolSubject.SCIENCE, 12);
     student2.addExam(SchoolSubject.CITIZENSHIP, 2);
+    student2.addExam(SchoolSubject.MUSIC, 10);
+    student2.addExam(SchoolSubject.MATH, new double[] {0.5, 0.5}, new int[] {7, 12});
 
     view.displayStudent(student2.getName(), student2.result());
     view.displayAverage(student2.calculateAverage());
